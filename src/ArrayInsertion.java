42071419b64ca92a26a9e3e6f14331e60554cc02
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-02-21 16:02:30 +0500

    I have added the insertAtFirst method and traverse method

---
+++ src/ArrayInsertion.java
@@ -0,0 +1,27 @@
+public class ArrayInsertion {
+    // traverse method
+    public static void traverse(int[] array) {
+        for (int i:array) {
+            System.out.print(i+" ");
+        }
+    }
+
+    // insertAtFirst
+    public static int[] insertAtFirst(int[] array,int value) {
+        int[] newArray = new int[array.length+1];
+        newArray[0] = value;
+        for (int i=1; i<newArray.length;  i++) {
+            newArray[i] = array[i-1];
+        }
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        traverse(array);
+        System.out.println();
+
+        array = insertAtFirst(array,21);
+        traverse(array);
+    }
+}
