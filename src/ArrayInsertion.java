118124fea8b212582b774fc8d330630d5e54c80c
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-02-21 16:37:09 +0500

    initial commit

--- src/ArrayInsertion.java
+++ src/ArrayInsertion.java
@@ -26,6 +26,52 @@ public class ArrayInsertion {
         return newArray;
     }
 
+    // insertAtMid
+    public static int[] insertAtMid(int[] array, int value) {
+        int[] newArray = new int[array.length+1];
+        int mid = (newArray.length-1) / 2;
+        newArray[mid] = value;
+        int count = 0;
+        for (int i=0; i<newArray.length; i++) {
+            if (i!=mid) {
+                newArray[i] = array[count];
+                count++;
+            }
+        }
+        return newArray;
+    }
+
+    // insertAtPosition
+    public static int[] insertAtPosition(int[] array, int value, int position) {
+        if (position<1 || position>array.length+1) throw new IllegalArgumentException("Exception: insert correct position");
+        int[] newArray = new int[array.length+1];
+        newArray[position-1] = value;
+        int count = 0;
+        for (int i=0; i<newArray.length; i++) {
+            if (i!=position-1) {
+                newArray[i] = array[count];
+                count++;
+            }
+        }
+        return newArray;
+    }
+
+    // insertAtIndex
+    public static int[] insertAtIndex(int[] array, int value, int  index) {
+        if (index<0 || index>array.length-1) throw new IllegalArgumentException("Exception: insert correct index");
+        int[] newArray = new int[array.length+1];
+        newArray[index] = value;
+        int count = 0;
+        for (int i=0; i<newArray.length; i++) {
+            if (i!=index) {
+                newArray[i] = array[count];
+                count++;
+            }
+        }
+        return newArray;
+    }
+
+    // main method
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5,6,7,8,9};
         traverse(array);
@@ -34,7 +80,16 @@ public class ArrayInsertion {
 //        array = insertAtFirst(array,21);
 //        traverse(array);
 
-        array = insertAtLast(array,21);
+//        array = insertAtLast(array,21);
+//        traverse(array);
+
+//        array = insertAtMid(array,21);
+//        traverse(array);
+
+//        array = insertAtPosition(array,21,7);
+//        traverse(array);
+
+        array = insertAtIndex(array,21,7);
         traverse(array);
     }
 }
