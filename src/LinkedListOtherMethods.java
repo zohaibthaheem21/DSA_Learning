8014d87f6b487c91a52e04d7df7ed2189de59adb
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-03-02 17:01:09 +0500

    I have added LinkedListOtherMethods file with highest, lowest, mid and frequency methods

--- src/LinkedListOtherMethods.java
+++ src/LinkedListOtherMethods.java
@@ -69,6 +69,46 @@ public class LinkedListOtherMethods {
         return (isAscending(head) || isDescending(head));
     }
 
+    // highest method
+    public int highest(Node head){
+        int highest=head.data;
+        for(Node n=head.next;n!=null;n=n.next){
+            if(highest<n.data)highest=n.data;
+        }
+        return highest;
+    }
+
+    // lowest method
+    public int lowest(Node head){
+        int lowest=head.data;
+        for(Node n=head.next;n!=null;n=n.next){
+            if(lowest>n.data)lowest=n.data;
+        }
+        return lowest;
+    }
+
+    // mid method
+    public int mid(Node head){
+        if(head==null)throw new IllegalArgumentException("LinkedList does not exist");
+        int mid=(1+size(head))/2;
+        Node temp=head;
+        for(int i=mid;i>1;i--){
+            temp=temp.next;
+        }
+        return temp.data;
+    }
+    public int frequency(Node head,int value) {
+        int count = 0;
+        for (Node n=head;n!=null; n=n.next) {
+            if (n.data==value) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // frequency
+
     // main method
     public static void main(String[] args) {
         LinkedListOtherMethods list = new LinkedListOtherMethods();
@@ -91,6 +131,14 @@ public class LinkedListOtherMethods {
 
 //        System.out.println(list.isDescending(head));
 
-        System.out.println(list.sorted(head));
+//        System.out.println(list.sorted(head));
+
+//        System.out.println(list.highest(head));
+
+//        System.out.println(list.lowest(head));
+
+//        System.out.println(list.mid(head));
+
+        System.out.println(list.frequency(head,10));
     }
 }
