1038973acb51568aaec048eb7bba5a9d82ab89a6
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-03-10 16:57:20 +0500

    I have added ArrayQueue with size, add, first add, remove and resize methods

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,3 +1,5 @@
+import java.security.PublicKey;
+
 public class ArrayQueue {
     private Object[] a;
     private int size;
@@ -13,12 +15,59 @@ public class ArrayQueue {
         return size == 0;
     }
 
+    // size()
+    public int size() {
+        return size;
+    }
 
+    // first()
+    public Object first() {
+        if (isEmpty()) throw new IllegalArgumentException("Exception");
+        return a[0];
+    }
+
+    // add()
+    public void add(Object data) {
+        if (size == a.length) resize();
+        a[size++] = data;
+    }
+
+    // remove()
+    public Object remove() {
+        if (isEmpty()) throw new IllegalArgumentException("Exception");
+        Object data = a[0];
+        System.arraycopy(a,1,a,0,--size);
+        return data;
+    }
+
+    // resize()
+    public void resize() {
+        Object[] aa = a;
+        a = new Object[size*2];
+        System.arraycopy(a,0,aa,0,size);
+    }
 
     // main method
     public static void main(String[] args) {
         ArrayQueue queue = new ArrayQueue(5);
 
-        System.out.println(queue.isEmpty());
+        System.out.println("isEmpty : "+queue.isEmpty());
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.first());
+            queue.remove();
+        }
+
+//        System.out.println("first : "+queue.first());
+
+//        System.out.println("Size : "+queue.size());
+
+//        System.out.println("remove : "+queue.remove());
     }
 }
